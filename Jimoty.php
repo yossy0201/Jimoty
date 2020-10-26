b0534818c8b41c97cebf2c9997ae821548bbe9a7
yossy0201 <yossy0201msk@gmail.com>
2020-10-26 22:51:13 +0900

    first-rogin-register

--- Jimoty.php
+++ Jimoty.php
@@ -13,7 +13,12 @@
       <div class="text">17093855件 10/26更新
         <a class="jimotonokeijiban-jimoty" href="https://jmty.jp/">地元の掲示板 ジモティー</a>
       <div class="first-rogin-regster">
-        
+        <a target="_first" class="u-margin-m-l" href="https://jmty.jp/about/what_is_jmty">初めての方へ</a>
+        <a target="_rogin" class="u-margin-l-l u-font-bold u-size-s" href="https://jmty.jp/users/sign_in">ログイン</a>
+        <div class="register">
+          <a target="_self" rel="nofollow" class="c-button-secondary u-padding-s" href="https://jmty.jp/users/sign_up?completed_return_to=https%3A%2F%2Fjmty.jp%2Fmy%2Fposts">会員登録</a>
+        </div>
+
       </div>
 
       </div>
