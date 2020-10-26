3583bf8e9da14db74b8f24f4e262541faf4620ec
yossy0201 <yossy0201msk@gmail.com>
2020-10-26 22:13:29 +0900

    Initial commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="mystyle.php">
+    <title>掲示板</title>
+  </head>
+  <body>
+  <h1>地元の掲示板ジモティー</h1>
+    <?php
+      echo "売ります・あげます!"
+    ?>
+  </body>
+</html>
---
+++ mystyle.php
@@ -0,0 +1,5 @@
+<?PHP
+// cssで記述する方法をセット
+header('Content-Type: text/css;charaset=utf-8');
+
+print "h1 {color:brown; }";
