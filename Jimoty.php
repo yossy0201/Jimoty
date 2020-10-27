30e0def595bb9638bbfa27ddd8e238e906739824
yossy0201 <yossy0201msk@gmail.com>
2020-10-28 00:37:13 +0900

    rink-color-blue-green

--- Jimoty.php
+++ Jimoty.php
@@ -2,23 +2,23 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="mystyle.php">
+    <link rel="stylesheet" type="text/css" href="mystyle.css">
     <title>ジモティー</title>
   </head>
 
   <body>
     <div class="header">
       <div class=jimotono-jimoty>
-        <div class="All">地元の掲示板ジモティー
+        <div class="jimoty">地元の掲示板ジモティー
           <div class="text">17093855件 10/26更新
-            <a class="jimotonokeijiban-jimoty" href="https://jmty.jp/">地元の掲示板 ジモティー</a>
+            <a class="link-gray" href="https://jmty.jp/">地元の掲示板 ジモティー</a>
           </div>
         </div>
         <div class="first-rogin-regster">
-          <a target="_first" class="first" href="https://jmty.jp/about/what_is_jmty">初めての方へ</a>
-          <a target="_rogin" class="rogin" href="https://jmty.jp/users/sign_in">ログイン</a>
+          <a target="_first" class="first-btn" href="https://jmty.jp/about/what_is_jmty">初めての方へ</a>
+          <a target="_rogin" class="rogin-btn" href="https://jmty.jp/users/sign_in">ログイン</a>
           <div class="register">
-            <a target="_self" rel="nofollow" class="c-button-secondary u-padding-s" href="https://jmty.jp/users/sign_up?completed_return_to=https%3A%2F%2Fjmty.jp%2Fmy%2Fposts">会員登録</a>
+            <a target="_self" rel="nofollow" class="regist-btn" href="https://jmty.jp/users/sign_up?completed_return_to=https%3A%2F%2Fjmty.jp%2Fmy%2Fposts">会員登録</a>
           </div>
         </div>
       </div>
