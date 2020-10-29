5199de2dbf320984ca15c32836c44e8606fc33f9
yossy0201 <yossy0201msk@gmail.com>
2020-10-29 22:26:57 +0900

    toppage

--- Jimoty.php
+++ Jimoty.php
@@ -2,65 +2,21 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="mystyle.css">
-    <title>ジモティー</title>
-  </head>
-
-  <body>
-    <div class="white_canvas">
-      <div class=p-header>
-        <div class="jimoty">地元の掲示板ジモティー
-          <div class="text">17093855件 10/26更新
-            <a class="link-gray" href="https://jmty.jp/">地元の掲示板 ジモティー</a>
-          </div>
-        </div>
-        <div class="first-rogin-regster">
-          <a target="_first" class="first-btn" href="https://jmty.jp/about/what_is_jmty">初めての方へ</a>
-          <a target="_rogin" class="rogin-btn" href="https://jmty.jp/users/sign_in">ログイン</a>
-          <div class="register">
-            <a target="_self" rel="nofollow" class="regist-btn" href="https://jmty.jp/users/sign_up?completed_return_to=https%3A%2F%2Fjmty.jp%2Fmy%2Fposts">会員登録</a>
-          </div>
-        </div>
-      </div>
-    </div>
-      
-      
-        <hr>
-        </hr>
-          <?php
-            echo "売ります・あげます"
-          ?>
-          <?php
-            echo "中古車"
-          ?>
-          <?php
-            echo "里親募集"
-          ?>
-          <?php
-            echo "メンバー募集"
-          ?>
-          <?php
-            echo "助け合い"
-          ?>
-          <?php
-            echo "アルバイト"
-          ?>
-          <?php
-            echo "正社員"
-          ?>
-          <?php
-            echo "教室・スクール"
-          ?>
-          <?php
-            echo "イベント"
-          ?>
-          <?php
-            echo "不動産"
-          ?>
-          <?php
-            echo "地元のお店"
-          ?>
-    </div>
-   
-  </body>
+    <link rel="stylesheet" type="text/css" href="jimoty.css">
+</head>
+<body>
+  <header>
+    <h7 class="title">地元の掲示板ジモティー</h7>
+    <nav class="nav">
+      <ul class="menu-group">
+        <li class="menu-item"><a href="#" style="text-decoration: none;">17098632件◯月◯日更新</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none;">地元の掲示板 ジモティー</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none;">初めての方へ</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none;">ログイン</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none;">会員登録</a></li>
+      </ul>
+    </nav>
+  </header>
+  <div class="b" style="background-image: url(https://d1d7kfcb5oumx0.cloudfront.net/assets/top/header_image2-42539bc4bd707bb7add924d4892d175a3096b65cb8cc231248799d43505c9844.png);width: 1450px;height: 445px;"></div>
+</body>
 </html>
