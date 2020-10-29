9d58564532a91054285d73e4728af086396b6491
yossy0201 <yossy0201msk@gmail.com>
2020-10-29 23:52:55 +0900

    header-nav

--- Jimoty.php
+++ Jimoty.php
@@ -6,29 +6,39 @@
 </head>
 <body>
   <header>
-    <h7 class="title">地元の掲示板ジモティー</h7>
+    <div class="title">地元の掲示板ジモティー</div>
     <div class="nav">
       <ul class="menu-group">
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: black;">17098632件◯月◯日更新</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: black;">17098632件 ◯月◯日更新</a></li>
         <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">地元の掲示板 ジモティー</a></li>
         <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">初めての方へ</a></li>
         <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">ログイン</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: green;">会員登録</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: green; font-weight: bold; border-radius: 4px; border: 1px solid; padding: 8px;">会員登録</a></li>
       </ul>
     </div>
- 
-
-    <!-- <div class="nav">
-      <ul class="menu-group">
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: black;">17098632件◯月◯日更新</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">地元の掲示板 ジモティー</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">初めての方へ</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">ログイン</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none; color: green;">会員登録</a></li>
-      </ul>
-    </div> -->
-    
   </header>
-  <div class="back-pic" style="background-image: url(https://d1d7kfcb5oumx0.cloudfront.net/assets/top/header_image2-42539bc4bd707bb7add924d4892d175a3096b65cb8cc231248799d43505c9844.png);"></div>
+  <header style="border-top: 1px solid gray;">
+    <div class="title" style="margin-right: 0px;"></div>
+    <div class="nav">
+      <ul class="menu-group">
+      <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">売ります・買います</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray; ">中古車</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">里親募集</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">メンバー募集</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">助け合い</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">アルバイト</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">正社員</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">教室・スクール</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">イベント</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">不動産</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">地元のお店</a></li>
+      </ul>
+    </div>
+  </header>
+  <div class="back-pic" style="background-image: url(https://d1d7kfcb5oumx0.cloudfront.net/assets/top/header_image2-42539bc4bd707bb7add924d4892d175a3096b65cb8cc231248799d43505c9844.png);">
+    <div class="pic-in">
+    </div>
+  </div>
+
 </body>
 </html>
