2926640a84818a62ba6bc4e1ec3eb8dde1a81b11
yossy0201 <yossy0201msk@gmail.com>
2020-10-29 22:45:22 +0900

    header-color

--- Jimoty.php
+++ Jimoty.php
@@ -7,16 +7,28 @@
 <body>
   <header>
     <h7 class="title">地元の掲示板ジモティー</h7>
-    <nav class="nav">
+    <div class="nav">
       <ul class="menu-group">
-        <li class="menu-item"><a href="#" style="text-decoration: none;">17098632件◯月◯日更新</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none;">地元の掲示板 ジモティー</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none;">初めての方へ</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none;">ログイン</a></li>
-        <li class="menu-item"><a href="#" style="text-decoration: none;">会員登録</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: black;">17098632件◯月◯日更新</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">地元の掲示板 ジモティー</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">初めての方へ</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">ログイン</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: green;">会員登録</a></li>
       </ul>
-    </nav>
+    </div>
+ 
+
+    <!-- <div class="nav">
+      <ul class="menu-group">
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: black;">17098632件◯月◯日更新</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: gray;">地元の掲示板 ジモティー</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">初めての方へ</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: blue;">ログイン</a></li>
+        <li class="menu-item"><a href="#" style="text-decoration: none; color: green;">会員登録</a></li>
+      </ul>
+    </div> -->
+    
   </header>
-  <div class="b" style="background-image: url(https://d1d7kfcb5oumx0.cloudfront.net/assets/top/header_image2-42539bc4bd707bb7add924d4892d175a3096b65cb8cc231248799d43505c9844.png);width: 1450px;height: 445px;"></div>
+  <div class="back-pic" style="background-image: url(https://d1d7kfcb5oumx0.cloudfront.net/assets/top/header_image2-42539bc4bd707bb7add924d4892d175a3096b65cb8cc231248799d43505c9844.png);"></div>
 </body>
 </html>
